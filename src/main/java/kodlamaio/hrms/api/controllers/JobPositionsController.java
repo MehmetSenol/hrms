f8b050d375768dce13cccdd85a2e1ca954205e28
Mehmet <mehmet.enol23@gmail.com>
2021-06-09 00:08:29 +0300

    hrms fronted güncelleme

--- src/main/java/kodlamaio/hrms/api/controllers/JobPositionsController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/JobPositionsController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/jobpositions")
 public class JobPositionsController {
     private JobPositionService jobPositionService;
